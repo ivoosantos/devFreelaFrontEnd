a51e61ebb49d100b4a570f3b60fec75c6683f8ab
ivoosantos <oliveiradossantosivo@gmail.com>
2024-04-16 21:30:13 -0300

    Trabalhando com formulários

--- devFleela/src/app/pages/register/register.component.ts
+++ devFleela/src/app/pages/register/register.component.ts
@@ -1,7 +1,8 @@
 // @ts-nocheck
 import { Component, OnInit } from '@angular/core';
-import { error } from 'console';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { msg } from 'src/app/shared/utils/msg';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,16 @@ import Swal from 'sweetalert2';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  msg = msg;
+  registerForm: FormGroup = this.fb.group({
+    role: ['', [Validators.required]],
+    fullName: ['', [Validators.required]],
+    birthdate: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
 
   ngOnInit(): void {
   }
@@ -28,65 +38,81 @@ export class RegisterComponent implements OnInit {
     return counter !== list.length;
 }
 
+toogleRole(role: 'dev'| 'cliente'){
+  this.registerForm.get('role')?.setValue(role);
+}
+
 cadastrar() {
+
+  if(this.registerForm.valid){
+    console.log(this.registerForm.value);
+  } else {
+    this.registerForm.markAllAsTouched();
+  }
+
     // Checa se alguma role foi checada.
-    if (this.checkIfAnyRoleIsChecked() === false) {
-      // alert('Marque alguma role!');
-      Swal.fire(
-            'Algo de errado...',
-            'Marque alguma role!',
-            'error'
-        )
-        return;
-    }
+    // if (this.checkIfAnyRoleIsChecked() === false) {
+    //   Swal.fire(
+    //         'Algo de errado...',
+    //         'Marque alguma role!',
+    //         'error'
+    //     )
+    //     return;
+    // }
 
     // Inicia a massa de dados (payload)
-    let payload = {
-        role: document.getElementsByName("role")[0].checked == true ? 'dev' : 'cliente',
-        fullName: document.querySelector("#fullName").value,
-        birthdate: document.querySelector("#birthdate").value,
-        email: document.querySelector("#email").value,
-        password: document.querySelector("#password").value
-    }
+    // let payload = {
+    //     role: document.getElementsByName("role")[0].checked == true ? 'dev' : 'cliente',
+    //     fullName: document.querySelector("#fullName").value,
+    //     birthdate: document.querySelector("#birthdate").value,
+    //     email: document.querySelector("#email").value,
+    //     password: document.querySelector("#password").value
+    // }
 
     // Enviar para API
-    fetch("https://622cd1e6087e0e041e147214.mockapi.io/api/users", {
-            method: 'POST',
-            body: JSON.stringify(payload),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(response => {
-          // alert('Cadastrado com sucesso!');
-          localStorage.setItem("userName", response.fullName);
-          localStorage.setItem("role", response.role === "dev" ? "Desenvolvedor" : "Cliente");
-          localStorage.setItem("idClient", response.id);
+    // fetch("https://622cd1e6087e0e041e147214.mockapi.io/api/users", {
+    //         method: 'POST',
+    //         body: JSON.stringify(payload),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //     .then(response => response.json())
+    //     .then(response => {
+    //       localStorage.setItem("userName", response.fullName);
+    //       localStorage.setItem("role", response.role === "dev" ? "Desenvolvedor" : "Cliente");
+    //       localStorage.setItem("idClient", response.id);
 
-          window.location.href = "list.html";  
-          Swal.fire({
-                title: 'Bom Trabalho!',
-                text: "Cadastrado com sucesso!",
-                icon: 'success',
-                confirmButtonText: 'Ok!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    localStorage.setItem("userName", response.fullName);
-                    localStorage.setItem("role", response.role === "dev" ? "Desenvolvedor" : "Cliente");
-                    localStorage.setItem("idClient", response.id);
+    //       window.location.href = "list.html";  
+    //       Swal.fire({
+    //             title: 'Bom Trabalho!',
+    //             text: "Cadastrado com sucesso!",
+    //             icon: 'success',
+    //             confirmButtonText: 'Ok!'
+    //         }).then((result) => {
+    //             if (result.isConfirmed) {
+    //                 localStorage.setItem("userName", response.fullName);
+    //                 localStorage.setItem("role", response.role === "dev" ? "Desenvolvedor" : "Cliente");
+    //                 localStorage.setItem("idClient", response.id);
 
-                    window.location.href = "list.html";
-                }
-            })
-        }).catch(error => {
-          Swal.fire(
-            'Algo de errado...',
-            ''+error+'',
-            'error'
-          )
-          return;
-        })
+    //                 window.location.href = "list.html";
+    //             }
+    //         })
+    //     }).catch(erro => {
+    //       Swal.fire(
+    //         'Algo de errado...',
+    //         ''+erro+'',
+    //         'error'
+    //       )
+    //       return;
+    //     })
+}
+
+isInvalid(inputName: string, validatorName: string){
+  const formControl: any = this.registerForm.get(inputName);
+  if(formControl.errors !== null){
+    return formControl.errors[validatorName] && formControl.touched;
+  }
 }
 
 }
--- devFleela/src/app/pages/register/register.module.ts
+++ devFleela/src/app/pages/register/register.module.ts
@@ -4,7 +4,9 @@ import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 import { LdButtonModule } from 'src/app/shared/components/ld-button/ld-button.module';
 import { LdWrapperModule } from 'src/app/features/ld-wrapper/ld-wrapper.module';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +17,10 @@ import { LdWrapperModule } from 'src/app/features/ld-wrapper/ld-wrapper.module';
     CommonModule,
     RegisterRoutingModule,
     LdButtonModule,
-    LdWrapperModule
+    LdWrapperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
 export class RegisterModule { }
